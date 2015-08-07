c0ca19f848ad925648d92c789113ef2b93c4835c
OctoD <iamoctod@gmail.com>
2015-08-07 11:42:34 +0200

    the new stylus task

---
+++ generators/task-stylus/templates/tasks/stylus.js
@@ -0,0 +1,29 @@
+module.exports.name = 'stylus';
+
+module.exports.build = function (config) {
+  var autoprefixer = require('gulp-autoprefixer');
+  var gulp = require('gulp');
+  var stylus = require('gulp-stylus');
+  var rideCss = require('ride-css');
+
+  var autoprefixerOptions = {
+    browsers: [
+      'safari > 5',
+      'firefox > 20'
+    ]
+  };
+
+  var stylusOptions = {
+    use: [rideCss()]
+  };
+
+  return function () {
+    gulp.src([
+      'stylus/**.styl',
+      '!stylus/_**.styl'
+    ])
+    .pipe(stylus(stylusOptions))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(config.__dirname));
+  };
+}
